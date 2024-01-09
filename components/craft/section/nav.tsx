c8e817d97a32cf6c13daf314bbd9ff1026c65e2a
Bridger Tower <bridgertower@gmail.com>
2024-01-09 13:58:23 -0700

    npm link

--- components/craft/section/nav.tsx
+++ components/craft/section/nav.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { Circle } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from '../../ui/button';
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Exporting craft components
+export * as Craft from './components/craft/layout';
+export { default as Footer } from './components/craft/section/footer';
+export { MobileNav } from './components/craft/section/mobile-nav';
+export { NavMenu } from './components/craft/section/nav-menu';
+export { default as Nav } from './components/craft/section/nav';
+export { ThemeProvider } from './components/craft/theme/theme-provider';
+export { ThemeToggle } from './components/craft/theme/theme-toggle';
