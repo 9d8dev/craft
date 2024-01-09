b4b45617cd65cd25dce330ea78fe0052146f8577
Bridger Tower <bridgertower@gmail.com>
2024-01-09 15:10:16 -0700

    updated

--- layout.tsx
+++ layout.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "./craft/theme/theme-provider";
+import { ThemeProvider } from "./theme/theme-provider";
 
 // LAYOUT
 // Layout Component
