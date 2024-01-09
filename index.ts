192cd72dea5a5399c6b7d3a571d36690c5a4b9a9
Bridger Tower <bridgertower@gmail.com>
2024-01-09 14:44:41 -0700

    updated

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
 export { Layout, Main, Section, Container, Article } from './components/craft/layout';
-export { default as Footer } from './components/craft/section/footer';
-export { MobileNav } from './components/craft/section/mobile-nav';
-export { NavMenu } from './components/craft/section/nav-menu';
-export { default as Nav } from './components/craft/section/nav';
-export { ThemeProvider } from './components/craft/theme/theme-provider';
-export { ThemeToggle } from './components/craft/theme/theme-toggle';
