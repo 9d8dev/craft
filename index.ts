25ddfc698dc73d0045e85c629a397263e4855ea3
Bridger Tower <bridgertower@gmail.com>
2024-01-09 14:15:07 -0700

    updates

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-// Exporting craft components
-export * as Craft from './components/craft/layout';
+export { Layout, Main, Section, Container, Article } from './components/craft/layout';
 export { default as Footer } from './components/craft/section/footer';
 export { MobileNav } from './components/craft/section/mobile-nav';
 export { NavMenu } from './components/craft/section/nav-menu';
